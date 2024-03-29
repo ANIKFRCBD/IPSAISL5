2d1ef88c948ccabc934b8a5c0c74b249cfc31a06
অনিক মল্লিক <anikmallick.frcbd@gmail.com>
2024-03-30 00:07:23 +0600

    first drop down added

--- IPSAISL5/TRM/views.py
+++ IPSAISL5/TRM/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render
 import pandas as pd
 from django.http import request
 from pathlib import Path
+import numpy as np
 
 
 # Create your views here.t
 source='DATASOURCE/BACS.csv'
 
 def trmpage(request):
-    datasource=datareading(request).to_html(index=False,escape=False)
-    context={'BACS': datasource}
+    datasource=datareading(request)
+    context={'BACS': datasource.tolist()}
+    print(context)
     return render(request,"trm.html",context)
 
 def datareading(request):
   # Assuming 'source' is the path to your CSV file
   print(request.method)
   bacs = pd.read_csv(source)
-  html_table = bacs.iloc[:,:]
+  html_table = bacs['Name']
  # Use key 'BACS' for consistency  # Optional, for debugging
   return html_table
 
